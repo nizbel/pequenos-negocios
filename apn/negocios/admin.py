ea13d73631fc03e85327daf5f94ca48345bc687f
nizbel <kingbowserii@gmail.com>
2020-03-28 01:53:44 -0300

    Adicionando models ao admin

--- apn/negocios/admin.py
+++ apn/negocios/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from apn.negocios.models import InfoPerfilUsuario
+from apn.negocios.models import InfoPerfilUsuario, NegocioUsuario, Produto
 
 # Define an inline admin descriptor for InfoPerfilUsuario model
 # which acts a bit like a singleton
@@ -25,3 +25,7 @@ admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
 admin.site.register(InfoPerfilUsuario)
+
+admin.site.register(NegocioUsuario)
+
+admin.site.register(Produto)
